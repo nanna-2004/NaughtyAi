0fa45faed8bb6b4a7d10db89b260ea191a10ddea
Pratyush Banerjee <159470963+nanna-2004@users.noreply.github.com>
2025-07-29 10:39:09 +0530

    Update AppSidebar.jsx

--- ai-video-generator/app/(main)/_components/AppSidebar.jsx
+++ ai-video-generator/app/(main)/_components/AppSidebar.jsx
@@ -57,7 +57,7 @@ function AppSidebar() {
       <SidebarHeader>
         <div className="flex items-center gap-2 w-full justify-center mt-5">
           <Image src="/logo.svg" alt="logo" width="40" height="40" />
-          <span className="font-bold text-2xl">Naughty AI</span>
+          <span className="font-bold text-2xl">Naught AI</span>
         </div>
       </SidebarHeader>
 
